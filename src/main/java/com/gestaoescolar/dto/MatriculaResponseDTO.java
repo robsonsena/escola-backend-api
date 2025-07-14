396f4344cee73f91d5bcf61247f7e136f89aff99
robsonsena <robsonsena@gmail.com>
2025-07-14 15:27:35 -0300

    Add MatriculaResponseDTO class and update MatriculaService imports

---
+++ src/main/java/com/gestaoescolar/dto/MatriculaResponseDTO.java
@@ -0,0 +1,31 @@
+package com.gestaoescolar.dto;
+
+public class MatriculaResponseDTO {
+    private Long id;
+    private Long alunoId;
+    private Long cursoId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(Long alunoId) {
+        this.alunoId = alunoId;
+    }
+
+    public Long getCursoId() {
+        return cursoId;
+    }
+
+    public void setCursoId(Long cursoId) {
+        this.cursoId = cursoId;
+    }
+}
